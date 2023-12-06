bec0122286d9610634fe0ed7cbbf10305c86ec9f
ffaassaa <107076427+ffaassaa@users.noreply.github.com>
2023-12-05 17:59:56 -0800

    Delete projectphase1sourcecode/src/main directory
    moved "main" directory to "application"

--- projectphase1sourcecode/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.redditclone {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-
-    opens com.example.redditclone to javafx.fxml;
-    exports com.example.redditclone;
-}
